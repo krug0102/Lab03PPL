868b890f499d91997c16326016b4f84f951bd36c
krug0102 <krug0102@morris.umn.edu>
2023-03-03 15:29:59 -0600

    Finish Task 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -21,9 +22,7 @@ typedef struct {
 date tomorrow(date);
 
 void printDate(date d) {
-    cout << (d.month < 10 ? "0" : "") << d.month << "/"
-         << (d.day < 10 ? "0" : "") << d.day << "/"
-         << d.year;
+    cout << d.month << "/" << d.day << "/" << d.year << endl;
 }
 
 // takes a date structure and returns the next date
@@ -32,7 +31,7 @@ date tomorrow(date d) {
 
     tomorrow.year = d.year;
     tomorrow.month = d.month;
-
+    tomorrow.day = d.day;
 
     switch (d.day) {
         case 28:
@@ -40,35 +39,57 @@ date tomorrow(date d) {
                 tomorrow.day = 1;
                 tomorrow.month = 3;
             } else {
-                tomorrow.day = d.day + 1;
+                tomorrow.day+=1;
             }
             break;
         case 30:
             if (d.month == 7 || d.month == 4 || d.month == 6 || d.month == 11) {
                 tomorrow.day = 1;
-                tomorrow.month = d.month + 1;
+                tomorrow.month+=1;
             } else
-                tomorrow.day = d.day + 1;
+                tomorrow.day+=1;
             break;
         case 31:
             if (d.month == 1 || d.month == 3 || d.month == 5 || d.month == 8 || d.month == 9 || d.month == 10) {
                 tomorrow.day = 1;
-                tomorrow.month = d.month + 1;
+                tomorrow.month+=1;
             }
             if (d.month == 12) {
                 tomorrow.day = 1;
                 tomorrow.month = 1;
-                tomorrow.year = d.year + 1;
+                tomorrow.year+=1;
             } else
-                tomorrow.day = d.day + 1;
+                tomorrow.day+=1;
             break;
         default:
-            tomorrow.day = d.day + 1;
+            tomorrow.day+=1;
             break;
     }
 
     return tomorrow;
 } // end tomorrow
+
+
+void printTempData(tempData arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << "The temperature was: " << arr[i].temp << " on ";
+        printDate(arr[i].date_);
+    }
+}
+
+tempData* averageTempData(tempData a1[], tempData a2[], int size)
+{
+    tempData *result = (tempData*) malloc(size * sizeof(tempData));
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = {a1[i].date_, (a1[i].temp + a2[i].temp)/2.0};
+    }
+
+    return result;
+}
+
 
 int main() {
 
@@ -112,6 +133,48 @@ int main() {
     printDate(d6);
     cout << " -> ";
     printDate(t6);
-    cout << "  expected output: 03/01/1900";
+    cout << "  expected output: 03/01/1900" << endl;
 
+    /*  Task 2
+ * Define a structure tempData that has a nested structure date (already defined) and a temperature reading (a double).
+ */
+
+// Question 1: Print out the size of this structure and write it down in comments.
+cout << sizeof(tempData) << endl;
+// 24 bytes
+
+// Question 2: Create two 5-element arrays of these structures (statically, not using malloc): highest and lowest,
+// representing the highest and the lowest temperature readings of the day. The dates for the two arrays should be the same.
+tempData h1 = {{2023, 01, 01}, 12.4};
+tempData l1 = {{2023, 01, 01}, 2.5};
+tempData h2 = {{2023, 01, 01}, 32.4};
+tempData l2 = {{2023, 01, 01}, 14.2};
+tempData h3 = {{2023, 01, 01}, 3.9};
+tempData l3 = {{2023, 01, 01}, -1.3};
+tempData h4 = {{2023, 01, 01}, 21.4};
+tempData l4 = {{2023, 01, 01}, 13.2};
+tempData h5 = {{2023, 01, 01}, 27.7};
+tempData l5 = {{2023, 01, 01}, 6.3};
+static tempData highest[5] = {h1,h2,h3,h4,h5};
+static tempData lowest[5] = {l1,l2,l3,l4,l5};
+
+// Question 3: Write a function that takes an array of tempData structures and it's size and prints it out in a human-readable format.
+// In comments for the function explain why it needs the size.
+cout << "highest: " << endl;
+printTempData(highest, 5);
+
+cout << "lowest: " << endl;
+printTempData(lowest,5);
+// printTempData needs the size because if you don't give it the size, it will use the pointer size, rather than the
+// size of the array.
+
+
+// Question 4: Write a function that takes two arrays of tempData and their size (assuming that they are the same size), allocates
+// a new array of tempData of the same size, and fills it with the average temperatures of the two arrays (make sure to set the dates as well).
+// The return type is the pointer to the new array.
+tempData *avgtemp;
+avgtemp = averageTempData(highest, lowest, 5);
+
+cout << "average: " << endl;
+printTempData(avgtemp, 5);
 }
